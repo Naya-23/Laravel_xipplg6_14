43a2e599a17ec13454150c06dfc8773f97e03c37
Naya-23 <isnaya.faizah@gmail.com>
2025-10-30 07:44:39 +0700

    Add Feature Show Student by ID

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -46,11 +46,10 @@ class StudentController extends Controller
     /**
      * Tampilkan detail siswa berdasarkan ID (Show)
      */
-    public function show($id)
-    {
-        $student = Student::findOrFail($id);
-        return view('admin.student.show', compact('student'));
-    }
+public function show(Student $student)
+{
+    return view('admin.student.show', compact('student'));
+}
 
     /**
      * Tampilkan form edit data siswa (Edit)
